6e71f5f3f0a6c41dc66807a00012a9fd31263e92
nagamani <nagamanichavaaws@gmail.com>
2023-03-07 06:08:29 +0000

    colon

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_docdb_subnet_group" "default" {
-  name       = ${var.env}-docdb-subnet-group"
+  name       = "${var.env}-docdb-subnet-group"
   subnet_ids = var.subnet_ids
 
   tags = merge(
