8b8d843bcda58cda9664524931f5d2ad3309bf70
nagamani <nagamanichavaaws@gmail.com>
2023-03-31 07:13:49 +0000

    user

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 data "aws_ssm_parameter" "DB_ADMIN_USER" {
-  name = "${var.env}.docDB.DB_ADMIN_USER"
+  name = "${var.env}.docdb.DB_ADMIN_USER"
 }
 data "aws_ssm_parameter" "DB_ADMIN_PASS" {
-  name = "${var.env}.docDB.DB_ADMIN_PASS"
+  name = "${var.env}.docdb.DB_ADMIN_PASS"
 }
 
 # data "aws_kms_key" "key" {
